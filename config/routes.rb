895a9fbd7f0d23ed6bf6e40cfbad32337bc4163a
vito <vito0705@163.com>
2017-04-22 18:41:53 +0800

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'adpages/adtest'
+  get '/ads' ,to: 'adpages#adtest'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
